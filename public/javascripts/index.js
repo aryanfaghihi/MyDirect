9cc821adb05b8730c5ae2fc7952db2973d422367
Aryan <aryanfaghihi@gmail.com>
2016-02-11 22:04:52 +1100

    Changes made to the view.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 var app = angular.module('project', []);
 app.controller('IndexAppController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
-
+  $scope.current_webpage = window.location.href;
 }]);
 
